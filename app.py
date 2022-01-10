05d8966f5a41214bc10ed362607be45b334fead0
act99 <dorxm999@gmail.com>
2022-01-10 21:41:49 +0900

    update

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
+import datetime
+import hashlib
+
 import bson
+import jwt
 from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
-
-import requests
-from bs4 import BeautifulSoup
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
 client = MongoClient('localhost', 27017)
 # client = MongoClient('mongodb://test:test@localhost', 27017)
 db = client.tastyseoul
+
+SECRET_KEY = '6조비밀키'
 
 ## HTML을 주는 부분
 @app.route('/')
@@ -24,6 +27,46 @@ def register():
 @app.route('/login')
 def login():
    return render_template('login.html')
+
+
+@app.route('/api/register', methods=['POST'])
+def api_register():
+    email_receive = request.form['email_give']
+    password_receive = request.form['password_give']
+    nick_receive = request.form['nick_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+
+    doc = {
+        "email":email_receive,
+        "password":pw_hash,
+        "nick":nick_receive,
+    }
+    db.users.insert_one(doc)
+
+    return jsonify({'msg':'저장이 완료되었습니다.'})
+
+
+@app.route('/api/login', methods=['POST'])
+def api_login():
+    email_receive = request.form['email_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+
+    result = db.users.find_one({'email':email_receive, 'password':pw_hash})
+
+    if result is not None:
+
+        payload = {
+            'email':email_receive,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=2)
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+        user
+
+
+
 
 # @app.route('/memo', methods=['GET'])
 # def listing():
