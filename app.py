88335fc986b6c507f0592bd8dfc2e091490c100a
act99 <dorxm999@gmail.com>
2022-01-10 21:51:12 +0900

    login register

--- app.py
+++ app.py
@@ -63,7 +63,9 @@ def api_login():
             'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=2)
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-        user
+        return jsonify({'result': 'success', 'token': token})
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 
 
