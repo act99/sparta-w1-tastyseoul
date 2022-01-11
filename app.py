24608c38e49d7e4f9ca67148cf10561dae5415fc
act99 <dorxm999@gmail.com>
2022-01-11 11:29:05 +0900

    register done

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import datetime
 import hashlib
-import bson
+# import bson
 import jwt
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 
 app = Flask(__name__)
 from pymongo import MongoClient
-from bson.objectid import ObjectId
+# from bson.objectid import ObjectId
 
 client = MongoClient('localhost', 27017)
 # client = MongoClient('mongodb://test:test@localhost', 27017)
@@ -37,6 +37,9 @@ def register():
 def login():
    return render_template('login.html')
 
+@app.route('/detail')
+def detail():
+   return render_template('detail.html')
 
 
 
@@ -53,17 +56,37 @@ def api_register():
         "password":pw_hash,
         "nick":nick_receive,
     }
-    # db.users.find({}, {"email"})
-    print("체크")
-    results = db.users.find({"email":"pootter@naver.com"})
-    for result in results:
-        if result['email'] != email_receive:
-            print("email이 없다")
-            return db.users.insert_one(doc)
-        else:
-            print("이메일이 있다.")
+    try:
+        db.users.find({"email":email_receive})[0]["email"]
+        return jsonify({'msg':"중복된 이메일이 있습니다."})
+    except:
+        db.users.insert_one(doc)
+        return jsonify({'msg':"저장이 완료되었습니다."})
 
-    return jsonify({'msg':'저장이 완료되었습니다.'})
+
+
+
+#
+# @app.route('/api/register', methods=['POST'])
+# def api_register():
+#     item_receive = request.form['item_receive']
+#
+#     doc = {
+#         "email":email_receive,
+#         "password":pw_hash,
+#         "nick":nick_receive,
+#     }
+#     # db.users.find({}, {"email"})
+#     print("체크")
+#     results = db.users.find({"email":"pootter@naver.com"})
+#     for result in results:
+#         if result['email'] != email_receive:
+#             print("email이 없다")
+#             db.users.insert_one(doc)
+#             return jsonify({'msg':"저장이 완료되었습니다."})
+#         else:
+#             print("이메일이 있다.")
+#             return jsonify({'msg': "중복된 이메일이 있습니다."})
 
 
 @app.route('/api/login', methods=['POST'])
@@ -111,67 +134,6 @@ def api_valid():
     except jwt.exceptions.DecodeError:
         return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
 
-
-# @app.route('/memo', methods=['GET'])
-# def listing():
-#     article_array = []
-#     articles = list(db.articles.find({}, {}))
-#     print(articles)
-#     for article in articles:
-#       id = str(article['_id'])
-#       title = article['title']
-#       image = article['image']
-#       desc = article['desc']
-#       url = article['url']
-#       comment = article['comment']
-#       doc = {
-#          "id": id,
-#          "title": title,
-#          "image": image,
-#          "desc": desc,
-#          "url": url,
-#          "comment": comment
-#       }
-#       article_array.append(doc)
-#     return jsonify({"all_articles":article_array})
-
-
-
-# ## API 역할을 하는 부분
-# @app.route('/memo', methods=['POST'])
-# def saving():
-#     url_receive = request.form['url_give']
-#     comment_receive = request.form['comment_give']
-
-#     headers = {
-#         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-#     data = requests.get(url_receive, headers=headers)
-
-#     soup = BeautifulSoup(data.text, 'html.parser')
-
-#     # 여기에 코딩을 해서 meta tag를 먼저 가져와보겠습니다.
-#     title = soup.select_one('meta[property="og:title"]')["content"]
-#     image = soup.select_one('meta[property="og:image"]')["content"]
-#     desc = soup.select_one('meta[property="og:description"]')["content"]
-
-#     doc = {
-#         "title":title,
-#         "image":image,
-#         "desc":desc,
-#         "url":url_receive,
-#         "comment":comment_receive
-#     }
-#     db.articles.insert_one(doc)
-
-#     return jsonify({'msg':'저장이 완료되었습니다.'})
-
-# @app.route('/memo/delete', methods=['POST'])
-# def delete():
-#    id_receive = request.form['id_give']
-#    set_id = ObjectId(id_receive)
-#    db.articles.delete_one({'_id': set_id})
-
-#    return jsonify({'msg': '삭제 완료!'})
 
 
 if __name__ == '__main__':
