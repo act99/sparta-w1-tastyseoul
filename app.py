7140df92a6d4c015f9ff826d5d9bdc1de851214d
act99 <dorxm999@gmail.com>
2022-01-11 17:09:55 +0900

    board done

--- app.py
+++ app.py
@@ -12,35 +12,41 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
 SECRET_KEY = 'SPARTA'
-
-# client = MongoClient('내AWS아이피', 27017, username="아이디", password="비밀번호")
+# client = MongoClient('mongodb://test:test@localhost', 27017)
 client = MongoClient('localhost', 27017)
 db = client.groupsix
 
+# //////////////////////////////////////////////////////
+# Front 라우터
 
+# 홈
 @app.route('/')
 def home():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-
-        return render_template('index.html')
+        user_info = db.users.find_one({"username": payload["id"]})
+        print("이거 체크")
+        print(user_info)
+        return render_template('index.html', user_info=user_info)
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
+# 디테일
 @app.route('/detail')
 def detail():
     msg = request.args.get("msg")
     return render_template('detail.html')
 
+# 로그인 & 회원가입
 @app.route('/login')
 def login():
     msg = request.args.get("msg")
     return render_template('login.html', msg=msg)
 
-
+# 프로필
 @app.route('/user/<username>')
 def user(username):
     # 각 사용자의 프로필과 글을 모아볼 수 있는 공간
@@ -55,6 +61,8 @@ def user(username):
         return redirect(url_for("home"))
 
 
+# //////////////////////////////////////////////////////
+# API
 @app.route('/sign_in', methods=['POST'])
 def sign_in():
     # 로그인
@@ -79,12 +87,13 @@ def sign_in():
 @app.route('/sign_up/save', methods=['POST'])
 def sign_up():
     username_receive = request.form['username_give']
+    profile_name_receive = request.form['profile_name_give']
     password_receive = request.form['password_give']
     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     doc = {
         "username": username_receive,                               # 아이디
         "password": password_hash,                                  # 비밀번호
-        "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
+        "profile_name": profile_name_receive,                           # 프로필 이름 기본값은 아이디
         # "profile_pic": "",                                          # 프로필 사진 파일 이름
         # "profile_pic_real": "profile_pics/profile_placeholder.png", # 프로필 사진 기본 이미지
         # "profile_info": ""                                          # 프로필 한 마디
@@ -98,6 +107,7 @@ def check_dup():
     username_receive = request.form['username_give']
     exists = bool(db.users.find_one({"username": username_receive}))
     return jsonify({'result': 'success', 'exists': exists})
+
 
 
 @app.route('/update_profile', methods=['POST'])
@@ -116,7 +126,17 @@ def posting():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        # 포스팅하기
+        user_info = db.users.find_one({"username": payload["id"]})
+        comment_receive = request.form["comment_give"]
+        date_receive = request.form["date_give"]
+        doc = {
+            "username": user_info["username"],
+            "profile_name": user_info["profile_name"],
+            "profile_pic_real": user_info["profile_pic_real"],
+            "comment": comment_receive,
+            "date": date_receive
+        }
+        db.posts.insert_one(doc)
         return jsonify({"result": "success", 'msg': '포스팅 성공'})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
@@ -142,6 +162,32 @@ def update_like():
         return jsonify({"result": "success", 'msg': 'updated'})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
+
+@app.route('/postfood', methods=['POST'])
+def save_post():
+    location_receive = request.form['location_give']
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+    file = request.files["file_give"]
+    extention = file.filename.split('.')[-1]
+    today = datetime.now()
+    mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
+
+    filename = f'file-{mytime}'
+
+    save_to = f'static/{filename}.{extention}'
+    file.save(save_to)
+
+    doc = {
+        'name':name_receive,
+        'location':location_receive,
+        'comment':comment_receive,
+        'file': f'{filename}.{extention}'
+    }
+
+    db.foodlist.insert_one(doc)
+
+    return jsonify({'msg': '저장 완료!'})
 
 
 if __name__ == '__main__':
